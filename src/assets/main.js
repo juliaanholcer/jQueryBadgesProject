08aa4e4984d83ed7d31bf364f85191cd7910ff32
juliaanholcer <julia.anholcer@interia.pl>
2017-08-19 13:00:36 +0200

    Bug fixed

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,34 +7,28 @@ $.ajax({
     success: function(response) {
       //take only Array that contains completed courses
       //console.log('response',response.courses.completed);
-      addCourses(response.courses.completed);
+    var $badges = $('#badges');
+    response.courses.completed.forEach(function(course) {
+      //div with .course class
+      var $course = $('<div class="course"></div>').appendTo($badges);
+
+      //h3 header with title attribute
+      $('<h3></h3>', {
+        text: course.title
+      }).appendTo($course);
+      //image with badge source
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+      //link with href = url, target attribute set as _blank , the btn & btn-primary CSS classes, See Course as its text contents
+      $('<a></a>', {
+        href: course.url,
+        target: '_blank',
+        'class': 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+    });
     }
 });
-
-//this function makes a content of the page based on what is in completed courses array
-var addCourses = function(coursesArray) {
-  var $badges = $('#badges');
-
-  coursesArray.forEach(function(course) {
-    //div with .course class
-    var $course = $('<div class="course"></div>').appendTo($badges);
-    //h3 header with title attribute
-    $('<h3></h3>', {
-      text: course.title
-    }).appendTo($course);
-    //image with badge source
-    $('<img />', {
-      src: course.badge
-    }).appendTo($course);
-    //link with href = url, target attribute set as _blank , the btn & btn-primary CSS classes, See Course as its text contents
-    $('<a></a>', {
-      href: course.url,
-      target: '_blank',
-      'class': 'btn btn-primary',
-      text: 'See Course'
-    }).appendTo($course);
-  });
-
-};
 
 });
