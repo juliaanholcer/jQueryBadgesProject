4b3583c8b15891bd3f1ee2c8fcdf449d54a3523c
juliaanholcer <julia.anholcer@interia.pl>
2017-08-19 12:27:11 +0200

    jQuery Badges Project Tasks 5-9

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,42 @@
 $(function() {
 
-  // your code will go here
+// Ajax request to the Report Card URL
+$.ajax({
+    url:'https://www.codeschool.com/users/julia_anholcer.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      //take only Array that contains completed courses
+      //console.log('response',response.courses.completed);
+      addCourses(response.courses.completed);
+    }
+});
+
+//this function makes a content of the page based on what is in completed courses array
+var addCourses = function(coursesArray) {
+  var $badges = $('#badges');
+
+  coursesArray.forEach(function(course) {
+    //div with .course class
+    var $course = $('<div></div>',{
+      'class':'course'
+    }).appendTo($badges);
+    //h3 header with title attribute
+    $('<h3></h3>', {
+      text: course.title
+    }).appendTo($course);
+    //image with badge source
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+    //link with href = url, target attribute set as _blank , the btn & btn-primary CSS classes, See Course as its text contents
+    $('<a></a>', {
+      href: course.url,
+      target: '_blank',
+      class: 'btn btn-primary',
+      text: 'See Course'
+    }).appendTo($course);
+  });
+
+};
 
 });
